7982fe55fca2fd9c98a3d3804daa40631be7f523
Joymaame <naanarockson@gmail.com>
2022-10-18 14:37:07 -0700

    My commit for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**main-prints the string phrase with proper grammar, but the outcome is a piece of art,
+ * Return : int 0 if exited right
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}

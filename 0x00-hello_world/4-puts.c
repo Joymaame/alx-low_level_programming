16b219db86cb1107717659b9546d0708f173b3f3
Joymaame <naanarockson@gmail.com>
2022-10-18 15:51:25 -0700

    My commitfor task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  Main print a set of string phrase
+ * Main print a set of string phrase
  * "Programming is like building a multilingual puzzle" 
- *  and returns the int value 0
+ * and returns the int value 0
    */
 
 int main(void)

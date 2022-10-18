ed35912c53f3dda25b7b68d3a2b8833a021d1282
Joymaame <naanarockson@gmail.com>
2022-10-18 13:31:54 -0700

    My commit for task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*Main print a set of string phrase and returns the int value 0*/
+
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

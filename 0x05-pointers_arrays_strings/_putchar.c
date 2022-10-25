16db88b74b4a6d8391a6456e9b723c747e594ffa
Joymaame <naanarockson@gmail.com>
2022-10-25 06:40:01 -0700

    Arrays

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+* Return: On success prints 1
+* on error it prints -1, and  the error number willbe set appropriately.
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

258ce261e6fe2c55734acfc2a665e51c389df07d
Joymaame <naanarockson@gmail.com>
2022-10-27 14:52:10 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -18,8 +18,10 @@ void print_number(int n)
 		n1 = n;
 	}
 
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
+	while (n1 / 10)
+	{
+		_putchar(n1 / 10);
+	else
+		_putchar((n1 % 10) + '0');
+	{
 }

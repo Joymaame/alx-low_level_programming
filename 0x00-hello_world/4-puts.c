3fa69b1eee2df0f9424c0be9ffcbce4aa6dc8a72
Joymaame <naanarockson@gmail.com>
2022-10-27 17:51:02 -0700

    C- tasks update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+
 /**
- * Main print a set of string phrase
- * "Programming is like building a multilingual puzzle" 
- * and returns the int value 0
-   */
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
